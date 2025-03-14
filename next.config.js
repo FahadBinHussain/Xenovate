fafebef627eb918210feb30cf22eaf3ec356f5f9
Fahad <fahadbinhussain001@gmail.com>
2025-03-14 21:46:28 +0600

    Update tsconfig.json to enhance module resolution by specifying baseUrl and adjusting path mappings for improved imports.

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': '.',
+    };
+    return config;
+  },
+}
+
+module.exports = nextConfig 
